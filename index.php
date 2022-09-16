e1461ab3a014745bc6cebbcdf97f003dd81ac3d1
Krazick02 <63378450+Krazick02@users.noreply.github.com>
2022-09-15 18:45:00 -0600

    Less

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="public/css/main.css">
+</head>
+<body>
+    <div class="container">
+        <fieldset>
+            <legend>Login</legend>
+            <div class="form">
+                <form action="">
+                    <div>
+                        <label for="">
+                            Correo:
+                            <input type="email" name="" id="" placeholder="usuario@correo.com">
+                        </label>
+                    </div>
+                    <div>
+                        <label for="">
+                            Contraseña:
+                            <input type="password" name="" id="">
+                        </label>
+                    </div>
+                    <button type="submit">Iniciar Secion</button>
+                </form>
+            </div>
+        </fieldset>
+    </div>
+</body>
+</html>
